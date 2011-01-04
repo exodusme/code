1e01685df03da34c1ab02434697c5da63304ba0f
Jaydev Mahadevan <jaydevm@counsyl.com>
2011-01-03 22:33:52 -0800

    initial commit. basic pre-launch landing page.

---
+++ server.js
@@ -0,0 +1,37 @@
+var sys = require("sys"),
+    http = require("http"),
+    url = require("url"),
+    path = require("path"),
+    fs = require("fs");
+
+var host = "localhost";
+var port = "8080";
+
+http.createServer(function(request, response) {
+    var uri = url.parse(request.url).pathname;
+    var filename = path.join(process.cwd(), uri);
+    path.exists(filename, function(exists) {
+	if (!exists) {
+	    response.writeHead(404, {"Content-Type": "text/plain"});
+	    response.write("404 Not Found\n");
+	    response.write(path + " does not exist on this site.\n");
+	    response.end();
+	    return;
+	}
+
+	fs.readFile(filename, "binary", function(err, file) {
+	    if (err) {
+		response.writeHead(500, {"Content-Type": "text/plain"});
+		response.write("500 Internal Server Error\n");
+		response.write(err + "\n");
+		response.end();
+		return;
+	    }
+	    response.writeHead(200);
+	    response.write(file, "binary");
+	    response.end();
+	});
+    });
+}).listen(port, host);
+
+sys.puts("Server running at " + host + ":" + port);
